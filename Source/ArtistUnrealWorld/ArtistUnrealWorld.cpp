dcfb22104b8250e665ff742744a5e3918caad261
yejianbin233 <yejianbin233@163.com>
2022-09-18 09:33:22 +0800

    init

---
+++ Source/ArtistUnrealWorld/ArtistUnrealWorld.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArtistUnrealWorld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArtistUnrealWorld, "ArtistUnrealWorld" );
---
+++ Source/ArtistUnrealWorld/ArtistUnrealWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArtistUnrealWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARTISTUNREALWORLD_API AArtistUnrealWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
